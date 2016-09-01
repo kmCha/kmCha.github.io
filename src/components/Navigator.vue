57aa034fc6d460b6dc9d78043cbc9a5db6ac2ac1
纠西 <majiuxi.chamajiuxi@alibaba-inc.com>
2016-09-02 01:23:24 +0800

    vue-router works

---
+++ src/components/Navigator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navigator">
+    <!-- 使用指令 v-link 进行导航。 -->
+    <a v-link="{ path: '/foo' }">Go to Foo</a>
+    <a v-link="{ path: '/bar' }">Go to Bar</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
+import Hello from './components/Hello'
 
 Vue.use(VueRouter)
 
+var router = new VueRouter()
+
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+// new Vue({
+//   el: 'body',
+//   components: { App }
+// })
+
+router.map({
+  '/foo': {
+    component: Hello
+  },
+  '/bar': {
+    component: Hello
+  }
 })
+
+router.start(App, 'body')
