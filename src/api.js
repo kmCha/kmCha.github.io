35d169b479c1c97f91526c57992e316a24637502
纠西 <majiuxi.chamajiuxi@alibaba-inc.com>
2016-09-04 02:09:22 +0800

    order articles by date

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 
 export function getArticles () {
-  return Vue.http.get('https://chamajiuxi.wilddogio.com/blog/posts.json')
+  return Vue.http.get('https://chamajiuxi.wilddogio.com/blog/posts.json', {
+    params: {
+      orderBy: '"date"',
+      limitToLast: 100
+    }
+  })
 }
 export function getCategories () {
   return Vue.http.get('https://chamajiuxi.wilddogio.com/blog/categories.json')
