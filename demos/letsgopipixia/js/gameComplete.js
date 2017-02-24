bef841fd19581eff6afd4c6ee9f6199016d8bfe3
kmCha <chamajiuxi@126.com>
2017-02-24 15:49:21 +0800

    皮皮虾游戏改版

--- demos/letsgopipixia/js/gameComplete.js
+++ demos/letsgopipixia/js/gameComplete.js
@@ -1,6 +1,6 @@
 var gameCompleteState = {
     preload: function (){
-        game.load.spritesheet('button', '../assets/button_sprite_sheet.png', 193, 71);
+        game.load.spritesheet('button', 'assets/button_sprite_sheet.png', 193, 71);
     },
     create: function() {
         var overText = game.add.text(WIDTH/2, HEIGHT/2, '完成时间：' + time + 's', { fontSize: '32px', fill: '#fff' });
